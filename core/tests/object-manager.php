6ba7b810d30c74bf5a804dcca3145b5413f2ec63
Aleksejs Kovalovs <aleks4444@inbox.lv>
2026-01-10 05:40:45 +0200

    Another phpstan improvement

---
+++ core/tests/object-manager.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+
+/** @psalm-suppress PossiblyUndefinedArrayOffset, PossiblyNullArgument */
+$kernel = new Kernel($_ENV['APP_ENV'], (bool) ($_ENV['APP_DEBUG'] ?? false));
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
